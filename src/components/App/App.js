97cea0232fc81e96e46d0a1f2b7f19ad911589bf
Asmita Gaire <asmitagaire00@gmail.com>
2022-02-15 17:54:36 +1100

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import Drum from '../Drum/Drum';
+import './App.css';
+
+function App() {
+  return (
+    <div className="drum-machine">
+      <Drum />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Drum/Drum.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Drum() {
+  return <div className="display"></div>;
+}
+
+export default Drum;
